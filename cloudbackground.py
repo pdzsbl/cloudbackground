b5e96914637beb6abfaf583b390c165de23bd909
pdzsbl <2823694360@qq.com>
2018-12-08 09:47:02 +0800

    on se class

--- cloudbackground.py
+++ cloudbackground.py
@@ -23,6 +23,26 @@ def registry():
 
     return resp
 
+@app.route('/firstpage/login')
+def login():
+    customerid = request.args.get("customerid")
+    password = request.args.get("password")
+    # login函数
+    if(True):
+        resp = make_response()
+        # resp.set_cookie('customerid', customerid)
+        # resp.set_cookie('password', password)
+        resp.response = render_template("index.html")
+
+        return resp
+    else:
+        return -1
+
+@app.route('/shoppingpage/getinfo')
+def shop():
+    customerid = request.cookies.get('customerid')
+    # getinfo function
+    
 
 
 if __name__ == '__main__':
